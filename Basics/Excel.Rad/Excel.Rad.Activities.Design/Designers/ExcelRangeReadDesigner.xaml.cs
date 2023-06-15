a648a2d44e8f2532b1be2ad693b29410398fb331
hasan-kayan <hasankayan2000@hotmail.com>
2023-06-15 13:09:21 +0300

    Will be get order

---
+++ Basics/Excel.Rad/Excel.Rad.Activities.Design/Designers/ExcelRangeReadDesigner.xaml.cs
@@ -0,0 +1,13 @@
+namespace Excel.Rad.Activities.Design.Designers
+{
+    /// <summary>
+    /// Interaction logic for ExcelRangeReadDesigner.xaml
+    /// </summary>
+    public partial class ExcelRangeReadDesigner
+    {
+        public ExcelRangeReadDesigner()
+        {
+            InitializeComponent();
+        }
+    }
+}
