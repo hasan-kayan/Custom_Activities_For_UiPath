e984dd3253c8683f4d352cf355e1d9588ce8da16
hasan-kayan <hasankayan2000@hotmail.com>
2023-06-17 00:05:28 +0300

    General Structure Created

--- Excel Without Creator/Excel Activate Self/Class1.cs
+++ Excel Without Creator/Excel Activate Self/Class1.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Activities;
+using System.ComponentModel;
+
 
 namespace Excel_Activate_Self
 {
     public class Class1
     {
+
     }
 }
