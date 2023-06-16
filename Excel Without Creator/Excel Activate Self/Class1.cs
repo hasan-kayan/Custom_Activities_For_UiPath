75c7a25c6fdca1d174dea032ca0ec9f3ac7f8f5b
hasan-kayan <hasankayan2000@hotmail.com>
2023-06-17 00:00:58 +0300

    Class Based Project

---
+++ Excel Without Creator/Excel Activate Self/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excel_Activate_Self
+{
+    public class Class1
+    {
+    }
+}
