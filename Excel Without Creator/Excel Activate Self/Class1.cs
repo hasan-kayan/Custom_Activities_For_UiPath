8243234a1b143b6bda5df94f7898a07104d38b14
hasan-kayan <hasankayan2000@hotmail.com>
2023-06-17 11:10:23 +0300

    Empty

--- Excel Without Creator/Excel Activate Self/Class1.cs
+++ Excel Without Creator/Excel Activate Self/Class1.cs
@@ -5,12 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Activities;
 using System.ComponentModel;
+using System.Runtime.InteropServices;
+using Microsoft.Office.Interop.Excel;
+ 
 
 
 namespace Excel_Activate_Self
 {
     public class Class1
     {
+
+
+
+
 
     }
 }
