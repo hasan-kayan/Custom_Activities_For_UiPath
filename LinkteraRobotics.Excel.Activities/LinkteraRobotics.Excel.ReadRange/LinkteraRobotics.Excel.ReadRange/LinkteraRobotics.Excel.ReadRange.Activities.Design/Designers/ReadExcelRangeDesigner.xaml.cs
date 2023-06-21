bb177cd80eb646ff537ab7a72b0a0a7a1d9cfe37
hasan-kayan <hasankayan2000@hotmail.com>
2023-06-21 14:01:24 +0300

    Clean Project

---
+++ LinkteraRobotics.Excel.Activities/LinkteraRobotics.Excel.ReadRange/LinkteraRobotics.Excel.ReadRange/LinkteraRobotics.Excel.ReadRange.Activities.Design/Designers/ReadExcelRangeDesigner.xaml.cs
@@ -0,0 +1,13 @@
+namespace LinkteraRobotics.Excel.ReadRange.Activities.Design.Designers
+{
+    /// <summary>
+    /// Interaction logic for ReadExcelRangeDesigner.xaml
+    /// </summary>
+    public partial class ReadExcelRangeDesigner
+    {
+        public ReadExcelRangeDesigner()
+        {
+            InitializeComponent();
+        }
+    }
+}
