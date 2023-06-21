c136549ae21839a478d308fccaee5100728f5066
hasan-kayan <hasankayan2000@hotmail.com>
2023-06-21 14:20:49 +0300

    Cleaned

--- LinkteraRobotics.Excel.Activities/LinkteraRobotics.Excel.ReadRange/LinkteraRobotics.Excel.ReadRange/LinkteraRobotics.Excel.ReadRange.Activities/Properties/SharedResources.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace UiPath.Shared.Localization
-{
-    internal class SharedResources : LinkteraRobotics.Excel.ReadRange.Activities.Properties.Resources
-    {
-    }
-}
--- LinkteraRobotics.Excel.Activities/LinkteraRobotics.Excel.ReadRange/LinkteraRobotics.Excel.ReadRange/LinkteraRobotics.Excel.ReadRange/Properties/SharedResources.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace UiPath.Shared.Localization
-{
-    internal class SharedResources : LinkteraRobotics.Excel.ReadRange.Properties.Resources
-    {
-    }
-}
