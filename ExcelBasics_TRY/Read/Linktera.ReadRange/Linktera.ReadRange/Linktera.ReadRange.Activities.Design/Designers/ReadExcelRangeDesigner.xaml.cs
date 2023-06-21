667e062cf25821767044a78ae40962cc2db923ff
hasan-kayan <hasankayan2000@hotmail.com>
2023-06-22 00:00:33 +0300

    Read Range Working

---
+++ ExcelBasics_TRY/Read/Linktera.ReadRange/Linktera.ReadRange/Linktera.ReadRange.Activities.Design/Designers/ReadExcelRangeDesigner.xaml.cs
@@ -0,0 +1,13 @@
+namespace Linktera.ReadRange.Activities.Design.Designers
+{
+    /// <summary>
+    /// Interaction logic for ReadExcelRangeDesigner.xaml
+    /// </summary>
+    public partial class ReadExcelRangeDesigner
+    {
+        public ReadExcelRangeDesigner()
+        {
+            InitializeComponent();
+        }
+    }
+}
