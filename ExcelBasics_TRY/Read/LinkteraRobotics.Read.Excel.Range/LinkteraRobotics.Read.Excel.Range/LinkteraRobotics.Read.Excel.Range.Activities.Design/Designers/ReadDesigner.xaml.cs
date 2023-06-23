227a0826102d05b10b1ea7108720a6fb3166342a
hasan-kayan <hasankayan2000@hotmail.com>
2023-06-23 10:13:09 +0300

    Fixing

---
+++ ExcelBasics_TRY/Read/LinkteraRobotics.Read.Excel.Range/LinkteraRobotics.Read.Excel.Range/LinkteraRobotics.Read.Excel.Range.Activities.Design/Designers/ReadDesigner.xaml.cs
@@ -0,0 +1,13 @@
+namespace LinkteraRobotics.Read.Excel.Range.Activities.Design.Designers
+{
+    /// <summary>
+    /// Interaction logic for ReadDesigner.xaml
+    /// </summary>
+    public partial class ReadDesigner
+    {
+        public ReadDesigner()
+        {
+            InitializeComponent();
+        }
+    }
+}
