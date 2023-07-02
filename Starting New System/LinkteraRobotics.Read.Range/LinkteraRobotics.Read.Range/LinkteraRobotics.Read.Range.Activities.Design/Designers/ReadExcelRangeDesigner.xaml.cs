e7ec6c37d9fae01adac98a9881e0f9d5309af19b
hasan-kayan <hasankayan2000@hotmail.com>
2023-07-02 20:22:51 +0300

    Read Range Created, 3 Arguments add,

---
+++ Starting New System/LinkteraRobotics.Read.Range/LinkteraRobotics.Read.Range/LinkteraRobotics.Read.Range.Activities.Design/Designers/ReadExcelRangeDesigner.xaml.cs
@@ -0,0 +1,13 @@
+namespace LinkteraRobotics.Read.Range.Activities.Design.Designers
+{
+    /// <summary>
+    /// Interaction logic for ReadExcelRangeDesigner.xaml
+    /// </summary>
+    public partial class ReadExcelRangeDesigner
+    {
+        public ReadExcelRangeDesigner()
+        {
+            InitializeComponent();
+        }
+    }
+}
