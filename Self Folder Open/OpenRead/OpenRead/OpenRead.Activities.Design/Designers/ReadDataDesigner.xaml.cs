e13c00ef553dcf25ceb6ccfb237ed670a067fd7c
hasan-kayan <hasankayan2000@hotmail.com>
2023-07-03 11:11:23 +0300

    new approach by new lib

---
+++ Self Folder Open/OpenRead/OpenRead/OpenRead.Activities.Design/Designers/ReadDataDesigner.xaml.cs
@@ -0,0 +1,13 @@
+namespace OpenRead.Activities.Design.Designers
+{
+    /// <summary>
+    /// Interaction logic for ReadDataDesigner.xaml
+    /// </summary>
+    public partial class ReadDataDesigner
+    {
+        public ReadDataDesigner()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Self Folder Open/OpenRead/OpenRead/OpenRead.Activities/Activities/ExcelDataSetConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace OpenRead.Activities
+{
+    internal class ExcelDataSetConfiguration
+    {
+        public ExcelDataSetConfiguration()
+        {
+        }
+
+        public System.Func<object, ExcelDataTableConfiguration> ConfigureDataTable { get; set; }
+    }
+}
