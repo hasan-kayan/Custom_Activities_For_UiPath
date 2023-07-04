445d5111ef0019f4400b5eb366ace56a3b0566ec
hasan-kayan <hasankayan2000@hotmail.com>
2023-07-04 17:42:43 +0300

    Created

---
+++ Read Range Force/LinkteraRobotics.Read.Range.Force/LinkteraRobotics.Read.Range.Force/LinkteraRobotics.Read.Range.Force.Activities.Design/Designers/ReadRangeForceDesigner.xaml.cs
@@ -0,0 +1,13 @@
+namespace LinkteraRobotics.Read.Range.Force.Activities.Design.Designers
+{
+    /// <summary>
+    /// Interaction logic for ReadRangeForceDesigner.xaml
+    /// </summary>
+    public partial class ReadRangeForceDesigner
+    {
+        public ReadRangeForceDesigner()
+        {
+            InitializeComponent();
+        }
+    }
+}
