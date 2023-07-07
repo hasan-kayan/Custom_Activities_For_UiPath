46eaee2b8e4aac47bccda3ab70c94d54a9d3aabe
hasan-kayan <hasankayan2000@hotmail.com>
2023-07-07 16:04:56 +0300

    No error copy paste

--- Copy Sheet/LinkteraRobotics.Copy.Sheet/LinkteraRobotics.Copy.Sheet.Activities/Activities/CopyExcelSheetValue.cs
+++ Copy Sheet/LinkteraRobotics.Copy.Sheet/LinkteraRobotics.Copy.Sheet.Activities/Activities/CopyExcelSheetValue.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 using LinkteraRobotics.Copy.Sheet.Activities.Properties;
 using UiPath.Shared.Activities;
 using UiPath.Shared.Activities.Localization;
+using Microsoft.Office.Interop.Excel;
+
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace LinkteraRobotics.Copy.Sheet.Activities
 {
@@ -59,10 +62,59 @@ namespace LinkteraRobotics.Copy.Sheet.Activities
             // Inputs
             var filepath = Filepath.Get(context);
             var sheetname = Sheetname.Get(context);
-    
+
             ///////////////////////////
             // Add execution logic HERE
             ///////////////////////////
+            ///
+
+            string excelFilePath = filepath;
+            string sheetName = sheetname;
+
+
+            // Excel uygulamasýný baþlat
+            Console.WriteLine("Starting Excel Application...");
+            Microsoft.Office.Interop.Excel.Application excelApp = new Microsoft.Office.Interop.Excel.Application();
+            excelApp.Visible = false;
+
+            // Aktif çalýþma kitabýný ve çalýþma sayfasýný al
+            Console.WriteLine("Active Workbook Detection");
+            Excel.Workbook workbook = excelApp.Workbooks.Open(excelFilePath);
+            Excel.Worksheet worksheet = (Worksheet)workbook.Sheets[sheetName];
+
+            try
+            {
+                Console.WriteLine("Selecting all cells...");
+                // Tüm hücreleri seç ve kopyala
+                Excel.Range cells = worksheet.Cells;
+                cells.Select();
+                cells.Copy();
+
+                // Yeni bir çalýþma sayfasý ekle
+                Console.WriteLine("Creating new sheet");
+                Excel.Worksheet newWorksheet = (Worksheet)workbook.Sheets.Add(After: workbook.ActiveSheet);
+
+                // Yapýþtýrma iþlemini gerçekleþtir
+                Console.WriteLine("Pasting data into new sheet...");
+                Excel.Range pasteRange = newWorksheet.Cells;
+                pasteRange.PasteSpecial(Excel.XlPasteType.xlPasteValues, Excel.XlPasteSpecialOperation.xlPasteSpecialOperationNone,
+                    SkipBlanks: false, Transpose: false);
+
+                Console.WriteLine("Process completed.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR: " + ex.Message);
+            }
+            finally
+            {
+                // Excel uygulamasýný kapat ve kaynaklarý serbest býrak
+                workbook.Close();
+                excelApp.Quit();
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(worksheet);
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(workbook);
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+            }
 
             // Outputs
             return (ctx) => {
