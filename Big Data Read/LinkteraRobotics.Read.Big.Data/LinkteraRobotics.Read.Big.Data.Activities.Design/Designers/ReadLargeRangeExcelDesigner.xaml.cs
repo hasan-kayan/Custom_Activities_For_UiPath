f0ecc578981cb7ff0b158bb92f1ece0c350fa689
hasan-kayan <hasankayan2000@hotmail.com>
2023-07-11 16:07:18 +0300

    big data read SUCCES BUİLD

---
+++ Big Data Read/LinkteraRobotics.Read.Big.Data/LinkteraRobotics.Read.Big.Data.Activities.Design/Designers/ReadLargeRangeExcelDesigner.xaml.cs
@@ -0,0 +1,13 @@
+namespace LinkteraRobotics.Read.Big.Data.Activities.Design.Designers
+{
+    /// <summary>
+    /// Interaction logic for ReadLargeRangeExcelDesigner.xaml
+    /// </summary>
+    public partial class ReadLargeRangeExcelDesigner
+    {
+        public ReadLargeRangeExcelDesigner()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Big Data Read/LinkteraRobotics.Read.Big.Data/LinkteraRobotics.Read.Big.Data.Activities/Activities/ReadLargeRangeExcel.cs
@@ -0,0 +1,237 @@
+using System;
+using System.Activities;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Data;
+using LinkteraRobotics.Read.Big.Data.Activities.Properties;
+using UiPath.Shared.Activities;
+using UiPath.Shared.Activities.Localization;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace LinkteraRobotics.Read.Big.Data.Activities
+{
+    [LocalizedDisplayName(nameof(Resources.ReadLargeRangeExcel_DisplayName))]
+    [LocalizedDescription(nameof(Resources.ReadLargeRangeExcel_Description))]
+    public class ReadLargeRangeExcel : ContinuableAsyncCodeActivity
+    {
+        #region Properties
+
+        /// <summary>
+        /// If set, continue executing the remaining activities even if the current activity has failed.
+        /// </summary>
+        [LocalizedCategory(nameof(Resources.Common_Category))]
+        [LocalizedDisplayName(nameof(Resources.ContinueOnError_DisplayName))]
+        [LocalizedDescription(nameof(Resources.ContinueOnError_Description))]
+        public override InArgument<bool> ContinueOnError { get; set; }
+
+        [LocalizedDisplayName(nameof(Resources.ReadLargeRangeExcel_Range_DisplayName))]
+        [LocalizedDescription(nameof(Resources.ReadLargeRangeExcel_Range_Description))]
+        [LocalizedCategory(nameof(Resources.Input_Category))]
+        public InArgument<string> Range { get; set; }
+
+        [LocalizedDisplayName(nameof(Resources.ReadLargeRangeExcel_Excelfilepath_DisplayName))]
+        [LocalizedDescription(nameof(Resources.ReadLargeRangeExcel_Excelfilepath_Description))]
+        [LocalizedCategory(nameof(Resources.Input_Category))]
+        public InArgument<string> Excelfilepath { get; set; }
+
+        [LocalizedDisplayName(nameof(Resources.ReadLargeRangeExcel_Sheetname_DisplayName))]
+        [LocalizedDescription(nameof(Resources.ReadLargeRangeExcel_Sheetname_Description))]
+        [LocalizedCategory(nameof(Resources.Input_Category))]
+        public InArgument<string> Sheetname { get; set; }
+
+        [LocalizedDisplayName(nameof(Resources.ReadLargeRangeExcel_OutputTable_DisplayName))]
+        [LocalizedDescription(nameof(Resources.ReadLargeRangeExcel_OutputTable_Description))]
+        [LocalizedCategory(nameof(Resources.Output_Category))]
+        public OutArgument<DataTable> OutputTable { get; set; }
+
+        #endregion
+
+
+        #region Constructors
+
+        public ReadLargeRangeExcel()
+        {
+        }
+
+        #endregion
+
+
+        #region Protected Methods
+
+        protected override void CacheMetadata(CodeActivityMetadata metadata)
+        {
+            if (Range == null) metadata.AddValidationError(string.Format(Resources.ValidationValue_Error, nameof(Range)));
+            if (Excelfilepath == null) metadata.AddValidationError(string.Format(Resources.ValidationValue_Error, nameof(Excelfilepath)));
+            if (Sheetname == null) metadata.AddValidationError(string.Format(Resources.ValidationValue_Error, nameof(Sheetname)));
+
+            base.CacheMetadata(metadata);
+        }
+
+        protected override async Task<Action<AsyncCodeActivityContext>> ExecuteAsync(AsyncCodeActivityContext context, CancellationToken cancellationToken)
+        {
+            // Inputs
+            var range = Range.Get(context);
+            var excelfilepath = Excelfilepath.Get(context);
+            var sheetname = Sheetname.Get(context);
+
+            // Added Variables Just because to not change structure
+
+            string excelFilePath = excelfilepath;
+            string sheetName = sheetname;
+
+            ///////////////////////////
+            // Add execution logic HERE
+            ///////////////////////////
+
+            Console.WriteLine("READING BIG DATA PROCESS !");
+            Console.WriteLine("Checking inputs: ");
+            Console.WriteLine("Target Excel File Path: " + excelfilepath);
+            Console.WriteLine("Target Excel Sheet: " + sheetName);
+            if (string.IsNullOrWhiteSpace(range))
+            {
+                Console.WriteLine("All data in current sheet will be read.");
+
+            }
+            else
+            {
+                Console.WriteLine("The range : " + range + " will be read.");
+
+            }
+
+
+            // STARTING EXCEL 
+
+            Console.WriteLine("Excel Application is Starting...");
+
+            Excel.Application excelApp = new Excel.Application();
+            excelApp.Visible = false;
+
+            Console.WriteLine("Workbook detection...");
+            Excel.Workbook workbook = excelApp.Workbooks.Open(excelFilePath);
+            Excel.Worksheet worksheet = null;
+            Excel.Worksheet newWorksheet = null;
+
+            try
+            {
+                Console.WriteLine("Process is starting.");
+                worksheet = (Excel.Worksheet)workbook.Sheets[sheetName];
+
+                Console.WriteLine("Copying Cells");
+                Excel.Range cells = worksheet.Cells;
+                cells.Copy();
+                Console.WriteLine("Cells Copied");
+
+                Console.WriteLine("Creating a temporary worksheet.");
+                newWorksheet = (Excel.Worksheet)workbook.Sheets.Add(After: workbook.ActiveSheet);
+                newWorksheet.Name = "Copied";
+                Console.WriteLine("Temporary sheet created ! ");
+
+                Excel.Range pasteRange = newWorksheet.Cells;
+                pasteRange.PasteSpecial(Excel.XlPasteType.xlPasteValues, Excel.XlPasteSpecialOperation.xlPasteSpecialOperationNone,
+                    SkipBlanks: false, Transpose: false);
+                Console.WriteLine("Special pasted values to temproray worksheet.");
+                Console.WriteLine("Checking range status.");
+
+                if (newWorksheet != null)
+                {
+                    Console.WriteLine("Data Table Creating, data reading...");
+                    Excel.Range excelRange;
+                    if (string.IsNullOrWhiteSpace(range))
+                    {
+                        Console.WriteLine("All data reading");
+                        excelRange = newWorksheet.UsedRange;
+                    }
+                    else
+                    {
+                        Console.WriteLine("The range : " + range + "is reading");
+                        excelRange = newWorksheet.Range[range];
+                    }
+
+                    int rowCount = excelRange.Rows.Count;
+                    int columnCount = excelRange.Columns.Count;
+                    Console.WriteLine("Data Table is Creating...");
+                    DataTable dataTable = new DataTable();
+
+                    // Read column names
+                    for (int col = 1; col <= columnCount; col++)
+                    {
+                        string columnName = excelRange.Cells[1, col]?.ToString() ?? $"Column{col}";
+
+                        dataTable.Columns.Add(columnName);
+                    }
+                    Console.WriteLine("Column names read.");
+
+                    // Read data in smaller chunks (rows)
+                    const int batchSize = 1000;
+                    int remainingRows = rowCount - 1;
+                    int startRow = 2;
+                    Console.WriteLine("Data is reading in chunks, this may take some time...");
+                    while (remainingRows > 0)
+                    {
+                        Console.WriteLine("Loop is starting...");
+                        int rowsToRead = Math.Min(batchSize, remainingRows);
+                        int endRow = startRow + rowsToRead - 1;
+
+                        Excel.Range dataRange = excelRange.Range[excelRange.Cells[startRow, 1], excelRange.Cells[endRow, columnCount]];
+                        object[,] excelData = (object[,])dataRange.Value;
+                        Console.WriteLine($"Read rows {startRow} to {endRow}");
+
+                        // Transfer data to the DataTable
+                        for (int row = 1; row <= rowsToRead; row++)
+                        {
+                            DataRow dataRow = dataTable.NewRow();
+                            for (int col = 1; col <= columnCount; col++)
+                            {
+                                dataRow[col - 1] = excelData[row, col];
+                            }
+                            dataTable.Rows.Add(dataRow);
+                        }
+                        Console.WriteLine($"Transferred rows {startRow} to {endRow}");
+
+                        startRow += batchSize;
+                        remainingRows -= rowsToRead;
+
+                        OutputTable.Set(context, dataTable);
+                        Console.WriteLine("Data table is ready to use !");
+                    }
+
+
+                }
+                else
+                {
+                    Console.WriteLine("New worksheet is null. Cannot proceed with data reading.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error occurred: {ex.Message}");
+                Console.WriteLine("Process terminated unsuccessfully");
+            }
+            finally
+            {
+                newWorksheet?.Activate();
+                workbook.Close(false); // False states that dont save changes !!!!!!!!!
+                excelApp.Quit();
+
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(newWorksheet);
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(worksheet);
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(workbook);
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+            }
+
+            Console.WriteLine("Process completed.");
+
+
+            // Outputs
+            return (ctx) => {
+                OutputTable.Set(ctx, OutputTable);
+            };
+        }
+
+        #endregion
+    }
+}
+
