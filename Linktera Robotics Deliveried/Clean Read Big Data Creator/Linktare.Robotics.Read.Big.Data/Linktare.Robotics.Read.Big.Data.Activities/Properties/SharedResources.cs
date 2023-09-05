e2472571a15b5c8604dba0e2d45c3a79e62e5f34
hasan-kayan <hasankayan2000@hotmail.com>
2023-09-05 01:16:23 -0700

    Unnecessary folders removed, new project created

--- Linktera Robotics Deliveried/Clean Read Big Data Creator/Linktare.Robotics.Read.Big.Data/Linktare.Robotics.Read.Big.Data.Activities/Properties/SharedResources.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace UiPath.Shared.Localization
-{
-    internal class SharedResources : Linktare.Robotics.Read.Big.Data.Activities.Properties.Resources
-    {
-    }
-}
--- Linktera Robotics Deliveried/Legacy Ext V3/Open File/LinkteraRobotics.LegacyV3.OpenExcel/LinkteraRobotics.LegacyV3.OpenExcel.Activities.Design/Properties/SharedResources.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace UiPath.Shared.Localization
-{
-    internal class SharedResources : LinkteraRobotics.LegacyV3.OpenExcel.Activities.Design.Properties.Resources
-    {
-    }
-}
